d7eef62654da179494c76760f89f7cd3556c7e9d
Aditya Moorjmalani <adityamoorjmalani@Adityas-MacBook-Air.local>
2026-05-08 00:45:44 +0530

    second

--- src/server.js
+++ src/server.js
@@ -4,11 +4,17 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
